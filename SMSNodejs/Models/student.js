1b8acb659e50fa004cb188f9060431da697a5c7c
HAFIZ SALAH UD DIN <salahudinhafiz@hotmail.com>
2018-11-03 18:48:09 +0500

    Add project files.

---
+++ SMSNodejs/Models/student.js
@@ -0,0 +1,16 @@
+﻿var mongoose = require('mongoose');
+//Define a schema
+var Schema = mongoose.Schema;
+
+var student = new Schema({
+    name: {
+        type: String,
+        required: [true, 'name is required']
+    },
+    rollno: {
+        type: String,
+        required: [true, 'rollno is required']
+    }
+});
+var StudetModel = mongoose.model('student', student);
+module.exports = StudetModel;
---
+++ SMSNodejs/routes/index.js
@@ -0,0 +1,19 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+//Import the mongoose module
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb://127.0.0.1/sms';
+mongoose.connect(mongoDB);
+// Get Mongoose to use the global promise library
+mongoose.Promise = global.Promise;
+//Get the default connection
+var db = mongoose.connection;
+/* GET home page. */
+router.get('/', function (req, res) {
+    db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+    res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
