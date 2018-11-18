bd9de4085040e603a8bfbd14187412751b5eaa3e
HAFIZ SALAH UD DIN <salahudinhafiz@hotmail.com>
2018-11-18 17:54:52 +0500

    update & delete flow

--- SMSNodejs/routes/index.js
+++ SMSNodejs/routes/index.js
@@ -12,6 +12,12 @@ router.get('/', function (req, res) {
 router.get('/AddStudent', function (req, res) { 
     res.render('AddStudent', { title: 'Express' });
 });
+router.get('/GetAllStudens', function (req, res) {
+    oStudent.find({}).then(function (students) {
+        res.render('GetAllStudens', { students });
+    });
+});
+//Api
 router.post('/AddStudent', function (req, res) {
     oStudent.create(req.body).then(function (student) {
         res.send({message:"successfully saved",isSaved:true});
@@ -27,14 +33,20 @@ router.get('/AllStudent', function (req, res) {
         res.send(students);
     });
 });
-router.get('/GetAllStudens', function (req, res) {
-    //oStudent.find({}).then(function (students) {
-    //    res.send(students);
-    //});
-    oStudent.find({}).then(function (students) {
-        res.render('GetAllStudens', { students });
+
+router.post('/DeleteStudent', function (req, res) {
+    oStudent.findByIdAndRemove({ _id: req.body.id }).then(function () {
+        res.send({ message: "successfully removed", isremoved: true });
     });
 });
+router.post('/UpdateStudent', function (req, res) {
+    oStudent.findByIdAndUpdate(req.body.id, req.body).then(function () {
+        res.send({ message: "successfully updated", isupdated: true });
+    });
+});
+
+
+
 
 
 
